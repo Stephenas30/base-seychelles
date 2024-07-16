12d03560d37c72f92a10c3e2fd7171f3e3f7af74
Stephanas <stephanas.dev@gmail.com>
2024-07-16 14:59:26 +0300

    Base laravel vue vite

---
+++ resources/js/components/About.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>Page about</h1>
+</template>
---
+++ resources/js/router/index.js
@@ -0,0 +1,27 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+import home from '../components/Home.vue';
+import about from '../components/About.vue';
+import notfound from '../components/NotFound.vue';
+
+const routes = [
+    {
+        path: '/',
+        component: home
+    },
+    {
+        path: '/about',
+        component: about
+    },
+    {
+        path: '/:pathMatch(.*)*',
+        component: notfound
+    }
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+});
+
+export default router;
